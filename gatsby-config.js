6d171e51978c743e0b918e8d16dc09a63ec33a6e
Bailey Latimer <baileylatimer@baileys-mbp.lan>
2020-09-18 17:40:42 -0400

    cloned starter repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,29 +5,29 @@ module.exports = {
     siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Bailey is a designer who codes.`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/latimer2k`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/baileylatimer`,
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/latimer2k`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
+        url: `https://www.linkedin.com/baileylatimer`,
       },
       {
         name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://dribbble.com/latimerdesign`,
       },
     ],
   },
